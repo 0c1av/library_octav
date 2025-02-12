cac9540aacfa63cb5abe189f5c447847006d7da0
0c1av <0c1avf@gmail.com>
2025-02-12 17:54:44 +0100

    Update README and package metadata for version 0.2.0; add usage examples and available styles

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="octav",  # Package name
-    version="0.1.0",  # Initial version
+    version="0.2.0",  # Initial version
     packages=find_packages(),  # Finds the octav/ folder
-    description="A simple text colorizer for terminal",
+    description="A simple text modifier library",  # Short description
     author="Camil Caudron",
     author_email="0c1avf@gmail.com",
     url="https://github.com/0c1av",  # Update with your GitHub link
